059837fc5c2b9cc258004ee18c9c1516bddb9f2c
Anton-Atrashkou <Anton_Atrashkou@epam.com>
2020-09-20 22:44:29 +0300

    homework-3

---
+++ src/loaders/index.js
@@ -0,0 +1,7 @@
+import expressLoader from "./express.loader";
+import sequelizeLoader from "./sequelize.loader";
+
+export default async ({ app }) => {
+  expressLoader(app);
+  await sequelizeLoader();
+};
